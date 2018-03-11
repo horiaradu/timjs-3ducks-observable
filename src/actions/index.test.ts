72633e1428ca847d12f4819da3aac6acbb4799f4
Horia Radu <horia.radu23@gmail.com>
2018-03-11 16:09:04 +0200

    test wip

--- src/actions/index.test.ts
+++ src/actions/index.test.ts
@@ -1,28 +1,32 @@
-// import * as actions from './index';
-// import * as types from './types';
+import { SET_DATA, fetchData, failWithDefaultHandler } from './types';
 
-// import configureStore from 'redux-mock-store';
-// import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
+import { createEpicMiddleware } from 'redux-observable';
+import rootEpic from './index';
 
-// const middlewares = [thunk];
-// const mockStore = configureStore(middlewares);
+function mockStore(...args: any[]) {
+  const epicMiddleware = createEpicMiddleware(rootEpic);
+  const middlewares = [epicMiddleware];
+  const mockStore = configureStore(middlewares);
+  return mockStore(...args);
+}
 
-// describe('project actions', () => {
-//   it('tests something', async () => {
-//     const store = mockStore();
-//     const res = await store.dispatch(actions.fetchSomeData());
-//     expect(res).toEqual('i got the data');
-//     const dispatched = store.getActions();
-//     expect(dispatched.length).toEqual(3);
-//     expect(dispatched[1]).toEqual({ type: types.SET_DATA, data: res });
-//   });
+describe('project actions', () => {
+  it('tests something', async () => {
+    const store = mockStore();
+    const res = await store.dispatch(fetchData());
+    expect(res).toEqual('i got the data');
+    const dispatched = store.getActions();
+    expect(dispatched.length).toEqual(3);
+    expect(dispatched[1]).toEqual({ type: SET_DATA, data: res });
+  });
 
-//   it('tests something else', async () => {
-//     const store = mockStore();
-//     const res = await store.dispatch(actions.failWithDefaultHandler());
-//     expect(res).toEqual(null);
-//     const dispatched = store.getActions();
-//     expect(dispatched.length).toEqual(3);
-//     expect(dispatched[1]).toEqual({ type: types.SET_DATA, data: res });
-//   });
-// });
+  it('tests something else', async () => {
+    const store = mockStore();
+    const res = await store.dispatch(failWithDefaultHandler());
+    expect(res).toEqual(null);
+    const dispatched = store.getActions();
+    expect(dispatched.length).toEqual(3);
+    expect(dispatched[1]).toEqual({ type: SET_DATA, data: res });
+  });
+});
